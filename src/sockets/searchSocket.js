9beaeb9a4837c920363aff92cf8e6bc7e3744a7c
Berkay Gündüz <gunduzberkay1532@gmail.com>
2025-02-27 13:45:23 +0300

    cont3

--- src/sockets/searchSocket.js
+++ src/sockets/searchSocket.js
@@ -1,27 +1,3 @@
-// const Domain = require("../models/domainModel");
-
-// const searchSocketHandler = (socket) => {
-//   socket.on("searchDomains", async (query) => {
-//     try {
-//       if (!query) {
-//         socket.emit("searchResults", []);
-//         return;
-//       }
-
-//       // MongoDB'de domainleri arama (case insensitive)
-//       const domains = await Domain.find({
-//         name: { $regex: query, $options: "i" },
-//       });
-
-//       socket.emit("searchResults", domains);
-//     } catch (error) {
-//       console.error("Arama hatası:", error);
-//     }
-//   });
-// };
-
-// module.exports = searchSocketHandler;
-
 const Domain = require("../models/domainModel");
 
 const searchSocketHandler = (socket) => {
@@ -46,7 +22,6 @@ const searchSocketHandler = (socket) => {
       if (!exactMatch) {
         domains.push({
           name: `${query}.lib`,
-          available: true, // Kullanıcının kaydedebileceğini belirtmek için
         });
       }
 
