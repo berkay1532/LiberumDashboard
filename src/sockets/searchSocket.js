6c91c0d48a845166098dc0cfc9799fa224072a43
Berkay Gündüz <gunduzberkay1532@gmail.com>
2025-02-28 14:50:52 +0300

    search socket fixed

--- src/sockets/searchSocket.js
+++ src/sockets/searchSocket.js
@@ -13,16 +13,15 @@ const searchSocketHandler = (socket) => {
         name: { $regex: `^${query}$`, $options: "i" },
       });
 
-      // Eğer tam olarak aynı isimde bir domain yoksa, öneri olarak ".lib" ekle
-      if (!exactMatch) {
-        domains.push({
-          name: `${query}.lib`,
-        });
-      }
       // İçinde geçenleri ara (tam eşleşme dahil)
       domains = await Domain.find({
         name: { $regex: query, $options: "i" },
       });
+
+      // Eğer tam olarak aynı isimde bir domain yoksa, öneri olarak ".lib" ekle
+      if (!exactMatch) {
+        domains.unshift({ name: `${query}.lib` });
+      }
 
       socket.emit("searchResults", domains);
     } catch (error) {
